a86fe3eaa1517bc6c55af61d433b467338fc4af5
Zahar778 <zax.kobra@gmail.com>
2024-02-06 20:00:15 +0200

    tz

--- src/container/index/index.js
+++ src/container/index/index.js
@@ -1,99 +1,71 @@
 class Slider {
-    static #content = null
-    static #left = null
-    static #right = null
-
-    static #count = 1
-    static #max = null
+    static #content = null;
+    static #left = null;
+    static #right = null;
+    static #count = 0;
+    static #max = null;
+    static #videosPerPage = 4;
 
     static init = () => {
-        this.#content = document.querySelector(
-            '.slider__content'
-        )
-        this.#left = document.querySelector(
-            '.slider__button--left'
-        )
-        this.#right = document.querySelector(
-            '.slider__button--right'
-        )
+        this.#content = document.querySelector('.slider__content');
+        this.#left = document.querySelector('.slider__button--left');
+        this.#right = document.querySelector('.slider__button--right');
+        this.#max = Math.ceil(this.#content.childElementCount / this.#videosPerPage);
 
-        this.#max = this.#content.childElementCount
+        this.#left.onclick = () => this.#slide('left');
+        this.#right.onclick = () => this.#slide('right');
 
-        this.#left.onclick = () => this.#slide('left')
-        this.#right.onclick = () => this.#slide('right')
-    }
+        const openPopups = document.querySelectorAll('.open-popup');
+        openPopups.forEach(popup => {
+            popup.addEventListener('click', () => {
+                const videoId = popup.parentElement.getAttribute('data-video-id');
+                Slider.openPopup(videoId);
+            });
+        });
+    };
 
-    static #slide = (slide) => {
-        const offsetWidth = this.#content.offsetWidth
-        const scrollLeft = this.#content.scrollLeft
-        const scrollWidth = this.#content.scrollWidth
+    static #slide = (direction) => {
+        const offsetWidth = this.#content.offsetWidth;
+        const totalVideos = this.#content.childElementCount;
 
-        let scroll = 0
-
-        if(side === 'left') {
-            if(this.#count === 1 || scrollLeft === 0) {
-                this.#count = this.#max
-                scroll = (this.#count - 1) * offsetWidth
-            }else {
-                this.#content -= 1
-                scroll = (this.#count - 1) * offsetWidth
-            }
+        if (direction === 'left') {
+            this.#count = (this.#count - 1 + this.#max) % this.#max;
+        } else if (direction === 'right') {
+            this.#count = (this.#count + 1) % this.#max;
         }
-        if(side === 'right') {
-            if(this.#count === scrollWidth - offsetWidth) {
-                this.#count = 1
-                scroll = 0
-            } else {
-                this.#count += 1
-                scroll = (this.#count - 1) * offsetWidth
-            }
-        }
+
+        const startPosition = this.#count * this.#videosPerPage;
+        const endPosition = Math.min(startPosition + this.#videosPerPage, totalVideos);
+
+        const scroll = startPosition * offsetWidth;
 
         this.#content.scrollTo({
             top: 0,
             left: scroll,
             behavior: 'smooth',
-        })
-    }
-}
-Slider.init()
-
-class Header {
-    static #heigth = null
-    static #wrapper = null
-    static #button = null
-
-    static #isOpen = falce
-
-    static init() {
-        this.#heigth = document.querySelector(
-            '.header__bottom',
-        ).offsetHeight
-
-        this.#wrapper = document.querySelector(
-            '.header__wrapper',
-        )
-        this.#button = document.querySelector('.header__button')
-
-        this.#button.onclick = this.#toggle
-    }
-
-    static #toggle = () => {
-        if(this.#isOpen) {
-            this.#button.classList.replace(
-                '.header__button--close',
-                '.header__button--open',
-            )
-            this.#wrapper.style.height = 0
-        }else {
-            this.#button.classList.replace(
-                '.header__button--open',
-                '.header__button--close',
-            )
-            this.#wrapper.style.height = `${this.#heigth}px`
-        }
-        this.#isOpen = !this.#isOpen
-    }
+        });
+    };
 }
 
-Header.init()
+Slider.init();
+
+
+let popupBg = document.querySelector('.popup__bg'); 
+let popup = document.querySelector('.popup'); 
+let openPopupButtons = document.querySelectorAll('.open-popup'); 
+
+
+openPopupButtons.forEach((button) => { 
+    button.addEventListener('click', (e) => { 
+        e.preventDefault(); 
+        popupBg.classList.add('active'); 
+        popup.classList.add('active'); 
+    })
+});
+
+document.addEventListener('click', (e) => { 
+    if(e.target === popupBg) { 
+        popupBg.classList.remove('active'); 
+        popup.classList.remove('active'); 
+    }
+});
